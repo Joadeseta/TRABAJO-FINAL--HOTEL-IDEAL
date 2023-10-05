b0436e5f36c9cf65869e66523b2c8e20a3737a60
Joa Deseta <113145368+Joadeseta@users.noreply.github.com>
2023-10-05 10:25:30 -0300

    Commit Entidad Huesped
    Commit Entidad Huesped

--- TRABAJO_FINAL_HOTELIDEAL/src/entidades/huesped.java
+++ TRABAJO_FINAL_HOTELIDEAL/src/entidades/huesped.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entidades;
 
 /**
@@ -10,5 +5,79 @@ package entidades;
  * @author joadz
  */
 public class huesped {
+
+    private int id;
+    private String nombre;
+    private String dni;
+    private String domicilio;
+    private String correo;
+    private String celular;
+
+    public huesped(String nombre, String dni, String domicilio, String correo, String celular) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.domicilio = domicilio;
+        this.correo = correo;
+        this.celular = celular;
+    }
+
+    public huesped(int id, String nombre, String dni, String domicilio, String correo, String celular) {
+        this.id = id;
+        this.nombre = nombre;
+        this.dni = dni;
+        this.domicilio = domicilio;
+        this.correo = correo;
+        this.celular = celular;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
     
+   
 }
+
