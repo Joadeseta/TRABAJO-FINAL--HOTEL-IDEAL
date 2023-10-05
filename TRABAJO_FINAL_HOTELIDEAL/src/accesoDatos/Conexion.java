a858664f2fefc071d678649de9daf10f1e11a908
Joa Deseta <113145368+Joadeseta@users.noreply.github.com>
2023-10-05 10:44:56 -0300

    Agregando conexionData
    Agregando conexionData

--- TRABAJO_FINAL_HOTELIDEAL/src/accesoDatos/Conexion.java
+++ TRABAJO_FINAL_HOTELIDEAL/src/accesoDatos/Conexion.java
@@ -5,10 +5,66 @@
  */
 package accesoDatos;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author joadz
  */
 public class Conexion {
+  
+
+    // URL de conexión a la base de datos (debe especificar el host y la base de datos)
+    private static final String URL = "jdbc:mariadb://localhost/";
+    private static final String DB = "g5eq81";   //Nombre de la base de datos
+    private static final String USUARIO = "root";// Nombre de usuario de la base de datos
+    private static final String PASSWORD = ""; // Contraseña de la base de datos
+
+    // Objeto de conexión a la base de datos
+    private static Connection connection;
+
     
-}
+    // Constructor privado (se utiliza un patrón Singleton)    
+    
+    private Conexion() {
+        
+// El constructor está vacío ya que no se desea crear instancias de esta clase.
+
+    }
+
+    
+    /**
+     * Método estático para obtener una conexión a la base de datos.
+     *
+     * @return Objeto Connection que representa la conexión a la base de datos.
+     */
+    
+    
+    public static Connection getConexion() {
+        if (connection == null) {
+            try {
+                
+                // Carga el controlador JDBC de MariaDB
+                
+                
+                Class.forName("org.mariadb.jdbc.Driver");
+                
+              // Configura y establece la conexión a la base de datos
+              
+                connection = DriverManager.getConnection(URL + DB
+                        + "?useLegacyDatetimeCode = false&serverTimezone = UTC"
+                        + "&user=" + USUARIO + "&password" + PASSWORD);
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error al conectarse a la DB: " + ex.getMessage());
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "Error al cargar los Drivers: " + ex.getMessage());
+            }
+        }
+        return connection;  // Devuelve la conexión a la base de datos
+    }
+
+} 
+
